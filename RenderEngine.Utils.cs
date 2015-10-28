ddc6bf439baeae05eac5a4d7a35bbafa522816cf
Nathan Letwory <nathan@mcneel.com>
2015-10-28 11:50:34 +0200

    Remove unused namespace imports.

--- RenderEngine.Utils.cs
+++ RenderEngine.Utils.cs
@@ -13,13 +13,12 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 **/
+
 using System;
 using System.Drawing;
 using ccl;
 using ccl.ShaderNodes;
-using Rhino;
 using Rhino.Render;
-using Rhino.Render.ChangeQueue;
 
 namespace RhinoCycles
 {
