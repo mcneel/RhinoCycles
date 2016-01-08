1fc90a368b238f92333c70e0db16aed644844ee3
Nathan Letwory <nathan@mcneel.com>
2016-01-08 11:55:47 +0200

    RhinoCycles: Tell Rhino Cycles doesn't support Shadow Only ground plane.

--- Plugin.cs
+++ Plugin.cs
@@ -165,6 +165,8 @@ namespace RhinoCycles
 		{
 			if (feature == RenderFeature.CustomDecalProperties)
 				return false;
+			if (feature == RenderFeature.ShadowOnlyGroundPlane)
+				return false;
 
 			return true;
 		}
