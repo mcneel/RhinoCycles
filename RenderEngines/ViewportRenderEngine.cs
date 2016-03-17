2312a195125c123967aa9a13a9c1ea5f3b12ffc8
Nathan Letwory <nathan@mcneel.com>
2016-03-17 10:50:22 +0200

    Move interactive rendering to Center.
    Interactive rendering isn't too fond of Hilbert Spiral, especially
    not in debug mode.
    
    Interactive rendering uses slicing when generating tiles, and slicing
    is currently not supported properly by the Hilbert Spiral approach.

--- RenderEngines/ViewportRenderEngine.cs
+++ RenderEngines/ViewportRenderEngine.cs
@@ -214,7 +214,7 @@ namespace RhinoCycles
 				Experimental = false,
 				Samples = samples,
 				TileSize = render_device.IsCpu ? new Size(32, 32) : new Size(256, 256),
-				TileOrder = TileOrder.HilbertSpiral,
+				TileOrder = TileOrder.Center,
 				Threads = (uint)(render_device.IsCpu ? cycles_engine.Settings.Threads : 0),
 				ShadingSystem = ShadingSystem.SVM,
 				StartResolution = render_device.IsCpu ? 16 : 64,
