3e5e27621ad7e18dd807d595feaa6c5889972290
Nathan Letwory <nathan@mcneel.com>
2017-03-28 11:26:07 +0300

    Add test commands to create and select multi device (CUDA).

---
+++ Commands/SelectMultiDevice.cs
@@ -0,0 +1,87 @@
+﻿/**
+Copyright 2014-2015 Robert McNeel and Associates
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+
+using System.Linq;
+using Rhino;
+using Rhino.Commands;
+using ccl;
+using RhinoCyclesCore.Core;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System.Collections.Generic;
+
+namespace RhinoCycles.Commands
+{
+	[System.Runtime.InteropServices.Guid("50988F79-779D-49CC-80EB-8D776A83D31B")]
+	[CommandStyle(Style.Hidden)]
+	public class SelectMultiDevice : Command
+	{
+		static SelectMultiDevice _instance;
+		public SelectMultiDevice()
+		{
+			if(_instance==null) _instance = this;
+		}
+
+		///<summary>The only instance of the SelectMultiDevice command.</summary>
+		public static SelectMultiDevice Instance => _instance;
+
+		public override string EnglishName => "TestSelectMultiDevice";
+
+		protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+		{
+			(PlugIn as Plugin)?.InitialiseCSycles();
+			var numDevices = Device.Count;
+			var endS = numDevices != 1 ? "s" : "";
+			RhinoApp.WriteLine($"We have {numDevices} device{endS}");
+			RhinoApp.WriteLine("----------");
+			List<int> allowedIds = new List<int>();
+			foreach (var dev in Device.Devices)
+			{
+				if (dev.Id >= 100000)
+				{
+					RhinoApp.WriteLine($"	Device {dev.Id}: {dev.Name} ({dev.Description})");
+					allowedIds.Add((int)dev.Id);
+				}
+			}
+			if(allowedIds.Count < 1)
+			{
+				RhinoApp.WriteLine("No multi-devices available");
+				return Result.Nothing;
+			}
+			var lowest = allowedIds.Min();
+			var highest = allowedIds.Min();
+			RhinoApp.WriteLine("----------");
+			var getNumber = new GetInteger();
+			getNumber.SetLowerLimit(lowest, false);
+			getNumber.SetUpperLimit(highest, false);
+			getNumber.SetDefaultInteger(lowest);
+			getNumber.SetCommandPrompt($"Select multi-device to render");
+			var getRc = getNumber.Get();
+			if (getNumber.CommandResult() != Result.Success) return getNumber.CommandResult();
+			if (getRc == GetResult.Number)
+			{
+				var idx = getNumber.Number();
+				Device dev = Device.GetDevice(idx);
+				RhinoApp.WriteLine($"User selected device {idx}: {dev}");
+				PlugIn.Settings.SetInteger("rc_renderdevice", idx);
+				RcCore.It.EngineSettings.SelectedDevice = idx;
+				return Result.Success;
+			}
+
+			return Result.Nothing;
+		}
+	}
+}
---
+++ Commands/TestCreateMultiCuda.cs
@@ -0,0 +1,95 @@
+﻿/**
+Copyright 2014-2015 Robert McNeel and Associates
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+**/
+
+using Rhino;
+using Rhino.Commands;
+using ccl;
+using RhinoCyclesCore.Core;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System.Collections.Generic;
+
+namespace RhinoCycles.Commands
+{
+	[System.Runtime.InteropServices.Guid("75B0608F-DDC9-4377-BB32-99218F227A32")]
+	[CommandStyle(Style.Hidden)]
+	public class TestCreateMultiCuda : Command
+	{
+		static TestCreateMultiCuda _instance;
+		public TestCreateMultiCuda()
+		{
+			if(_instance==null) _instance = this;
+		}
+
+		///<summary>The only instance of the TestCreateMultiCuda command.</summary>
+		public static TestCreateMultiCuda Instance => _instance;
+
+		public override string EnglishName => "TestCreateMultiCuda";
+
+		protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+		{
+			(PlugIn as Plugin)?.InitialiseCSycles();
+			var numDevices = Device.Count;
+			var endS = numDevices != 1 ? "s" : "";
+			RhinoApp.WriteLine("Possible devices to select from:");
+			HashSet<int> allowedIds = new HashSet<int>();
+			foreach (var dev in Device.Devices)
+			{
+				if (dev.IsCpu || dev.IsCuda)
+				{
+					RhinoApp.WriteLine($"	Device {dev.Id}: {dev.Name} ({dev.Description})");
+					allowedIds.Add((int)dev.Id);
+				}
+			}
+			var getString = new GetString();
+			getString.SetCommandPrompt($"Enter comma-delimited string");
+			var getRc = getString.Get();
+			if (getString.CommandResult() != Result.Success) return getString.CommandResult();
+			if (getRc == GetResult.String)
+			{
+				var res = getString.StringResult();
+				var cleaned = res.Trim().ToLowerInvariant().Split(',');
+				HashSet<int> set = new HashSet<int>();
+				foreach(var item in cleaned)
+				{
+					int x;
+					if(int.TryParse(item, out x))
+					{
+						set.Add(x);
+					}
+				}
+
+				set.IntersectWith(allowedIds);
+				List<int> idList = new List<int>();
+				foreach (var s in set) idList.Add(s);
+				idList.Sort();
+
+				List<Device> devList = new List<Device>();
+				foreach(var id in idList)
+				{
+					devList.Add(Device.GetDevice(id));
+				}
+
+				var multiDevice = Device.CreateMultiDevice(devList);
+				RhinoApp.WriteLine($"	Device {multiDevice.Id}: {multiDevice.Name} ({multiDevice.Description})");
+
+				return Result.Success;
+			}
+
+			return Result.Nothing;
+		}
+	}
+}
