226dd660f6d18fd2000f5b98f92d794c06cc1718
Nathan Letwory <nathan@mcneel.com>
2018-08-29 12:32:00 +0300

    Ground plane texture rotation was interpreted wrong.

--- Database/ChangeDatabase.cs
+++ Database/ChangeDatabase.cs
@@ -1237,7 +1237,7 @@ namespace RhinoCyclesCore.Database
 			var motion = new Rhino.Geometry.Vector3d(gp.TextureOffset.X, gp.TextureOffset.Y, 0.0);
 			var ttrans = Rhino.Geometry.Transform.Translation(motion);
 			tfm *= ttrans;
-			var trot = Rhino.Geometry.Transform.Rotation(gp.TextureRotation, pp);
+			var trot = Rhino.Geometry.Transform.Rotation(Rhino.RhinoMath.ToRadians(gp.TextureRotation), pp);
 			tfm *= trot;
 			var texturemapping = TextureMapping.CreatePlaneMapping(pmap, smext, smext, smext);
 			if (texturemapping != null)
