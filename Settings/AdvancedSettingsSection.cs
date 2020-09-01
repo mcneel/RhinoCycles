8cd67630e39701deb64c8a28b36d297f3f018895
Bozo <bozo@mcneel.com>
2020-09-01 11:14:12 -0700

    Update localization strings

--- Settings/AdvancedSettingsSection.cs
+++ Settings/AdvancedSettingsSection.cs
@@ -74,7 +74,7 @@ namespace RhinoCyclesCore.Settings
 		public AdvancedSettingsSection(Guid pluginId, bool for_app, uint doc_serial) : base(for_app, doc_serial)
 		{
 			m_pluginId = pluginId;
-			m_caption = LOC.COV("Rhino Render Advanced Settings");
+			m_caption = Localization.LocalizeCommandOptionValue("Rhino Render Advanced Settings", 38);
 			InitializeComponents();
 			InitializeLayout();
 			RegisterControlEvents();
